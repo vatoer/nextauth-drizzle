121f93f664cedbb5a1c0ea932d370953ba1340ab
vatoer <vatoer.ckplus@gmail.com>
2024-04-24 12:56:03 +0200

    signin with google

---
+++ src/app/(auth)/signin/page.tsx
@@ -0,0 +1,12 @@
+import { SignIn } from "@/components/sign-in";
+
+const SigninPage = () => {
+  return (
+    <div>
+      Signin Page
+      <SignIn />
+    </div>
+  );
+};
+
+export default SigninPage;
